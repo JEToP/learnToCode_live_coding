21152a95a2b07cd7ff342f19cf2f57613f46c89b
Andrea <andreademasi80@gmail.com>
2021-11-12 13:34:33 +0100

    Added form logging

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,26 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useState } from "react";
 
 import Section from "../atoms/section/Section";
 import styles from "./contact.module.css";
 
 const Contact = memo(() => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = useCallback(
+    (event) => {
+      console.log(`
+      Email: ${email}
+      Name: ${name}
+      Message: ${message}
+    `);
+
+      event.preventDefault();
+    },
+    [email, message, name]
+  );
+
   return (
     <div className={styles.wrapper}>
       <Section grid className={styles.section}>
@@ -15,30 +32,35 @@ const Contact = memo(() => {
           </p>
         </div>
         <div className={styles.form}>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <label htmlFor="">Name</label>
             <input
               type="text"
-              name=""
+              name="name"
               id=""
               className={styles.input}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               required
             />
             <label htmlFor="">Email Address</label>
             <input
               type="email"
-              name=""
+              name="email"
               id=""
               className={styles.input}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
             <label htmlFor="">Message</label>
             <textarea
-              name=""
-              id=""
+              name="message"
               cols="30"
               rows="10"
               className={styles.input}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               required
             />
             <input
